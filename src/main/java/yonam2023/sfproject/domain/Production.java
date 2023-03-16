ac3d29dc96e98b9000a25e1f088a7f2c7bd02222
Ljh3141 <crido3141@gmail.com>
2023-03-16 21:41:30 +0900

    [#2] add production model
    add model of production part(additional change available)
    add dependency of lombok, javax
    
    close #2

--- src/main/java/yonam2023/sfproject/domain/Production.java
+++ src/main/java/yonam2023/sfproject/domain/Production.java
@@ -1,4 +1,36 @@
 package yonam2023.sfproject.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="PRODUCTION")
+@Getter
+@Setter
 public class Production {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "STYPE")
+    private String stype;
+
+    @Column(name = "SVALUE")
+    private int svalue;
+
+    @Column(name = "OTIME")
+    @CreatedDate
+    private Date otime;
+
+    @Builder
+    public Production(String stype, int svalue){
+        this.stype = stype;
+        this.svalue = svalue;
+    }
 }
