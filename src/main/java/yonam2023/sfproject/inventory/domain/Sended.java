fc35990becac9b39af6d0a6973eeb37c219c4504
JongDeu <ghk4889@naver.com>
2023-03-21 10:26:16 +0900

    [#3] add Sended.java

---
+++ src/main/java/yonam2023/sfproject/inventory/domain/Sended.java
@@ -0,0 +1,24 @@
+package yonam2023.sfproject.inventory.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Sended {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private int amount;
+
+    private LocalDateTime localDateTime;
+}
