9079a10e7ca9139699b05f8bfa1f3d0c9b91df4b
JongDeu <ghk4889@naver.com>
2023-04-05 14:32:38 +0900

    [#20] add pagination & edit structure storedItems.html

--- src/main/java/yonam2023/sfproject/logistics/controller/StoredItemViewController.java
+++ src/main/java/yonam2023/sfproject/logistics/controller/StoredItemViewController.java
@@ -3,10 +3,15 @@ package yonam2023.sfproject.logistics.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import yonam2023.sfproject.logistics.domain.SendRecord;
 import yonam2023.sfproject.logistics.domain.StoredItem;
 import yonam2023.sfproject.logistics.repository.StoredItemRepository;
 
@@ -20,10 +25,12 @@ import java.util.List;
 public class StoredItemViewController {
     private final StoredItemRepository storedItemRepo;
 
+
     @GetMapping
-    public String storedItemsHome(Model model){
-        List<StoredItem> storedItems = storedItemRepo.findAll();
-        model.addAttribute("storedItems", storedItems);
+    public String storedItemsHome(Model model, @PageableDefault Pageable pageable){
+        Page<StoredItem> all = storedItemRepo.findAll(pageable);
+        model.addAttribute("pageObj", all);
+
         return "logistics/storedItems";
     }
 
