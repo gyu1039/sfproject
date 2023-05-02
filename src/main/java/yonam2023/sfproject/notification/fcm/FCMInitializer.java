f75e7e53a983efe7ab21aaacc9d9cdd341b7efb9
JongDeu <ghk4889@naver.com>
2023-05-02 19:40:35 +0900

    [#32] add FCM 알림 기능 기본 세팅

---
+++ src/main/java/yonam2023/sfproject/notification/fcm/FCMInitializer.java
@@ -0,0 +1,35 @@
+package yonam2023.sfproject.notification.fcm;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@Service
+public class FCMInitializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(FCMInitializer.class);
+    private static final String FIREBASE_CONFIG_PATH = "notification-797fc-firebase-adminsdk-4cfsn-c8c024e648.json";
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream())).build();
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                logger.info("Firebase application has been initialized");
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+}
+
